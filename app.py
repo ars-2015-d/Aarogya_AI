ea389ca7da64c3ff5b90c60824ef945ff0f5031b
@rs <allirohith@MacBookAir.lan>
2026-08-25 12:49:14 +0530

    Fix macOS crash flags, dependencies, and stable triage workflow

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "1"
+
 import gc
 import faiss
 import numpy as np
